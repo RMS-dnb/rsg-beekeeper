26c93f3ef2744478aa106360f88890c06e53ffe0
RexShack <rexshackuk@gmail.com>
2023-02-13 18:00:08 +0000

    Initial commit

---
+++ config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+-- bee_house_gk_1
+-- bee_house_gk_2
+-- bee_house_gk_3
+-- bee_house_gk_4
+-- bee_house_gk_5
+-- bee_house_gk_6
+
+-- setup beehives
+Config.BeeHives = {
+    { lable = 'BeeHive', name = 'beehive1', coords = vector3(-467.33, 862.36, 126.91), heading = 268.1,  model = 'bee_house_gk_2', showblip = true },
+    { lable = 'BeeHive', name = 'beehive2', coords = vector3(-468.93, 859.02, 126.77), heading = 246.86, model = 'bee_house_gk_3', showblip = true },
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,32 @@
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+
+author 'RexShack#3041'
+description 'rsg-beekeeper'
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
+
+dependency 'rsg-core'
+
+files {
+    'stream/bee_house_gk_ytyp.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/bee_house_gk_ytyp.ytyp'
+
+escrow_ignore {
+    'stream/*/*.ydr'
+}
+
+lua54 'yes'
