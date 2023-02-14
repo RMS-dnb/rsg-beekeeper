14e531a53d8a3455d8ddb805ad3f99f709917708
RexShack <rexshackuk@gmail.com>
2023-02-14 12:44:33 +0000

    config + tidy

--- config.lua
+++ config.lua
@@ -7,6 +7,15 @@ Config = {}
 -- bee_house_gk_5
 -- bee_house_gk_6
 
+Config.BeeHiveProps = {
+    `bee_house_gk_1`,
+    `bee_house_gk_2`,
+    `bee_house_gk_3`,
+    `bee_house_gk_4`,
+    `bee_house_gk_5`,
+    `bee_house_gk_6`,
+}
+
 -- setup beehives
 Config.BeeHives = {
     { lable = 'BeeHive', name = 'beehive1', coords = vector3(-467.33, 862.36, 126.91), heading = 268.1,  model = 'bee_house_gk_2', showblip = true },
